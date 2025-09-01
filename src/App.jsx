8e6f28395a5b181039ff83c0655ca36cd52f4441
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2025-09-01 00:13:46 -0600

    Added: react-router-doom && xiao page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
+import Xiao from './Pages/Xiao'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <Router>
+      <Routes>
+        <Route path='/' element={<Xiao />} />
+      </Routes>
+    </Router>
     </>
   )
 }
---
+++ src/Pages/Xiao.jsx
@@ -0,0 +1,8 @@
+
+function Xiao() {
+  return (
+    <div className="bg-red-900">Xiao</div>
+  )
+}
+
+export default Xiao
