4b30c6a2546ee558c0e207ee984ea0efbabf2c16
Miguel Angel Gtz Gomez <233382@ids.upchiapas.edu.mx>
2025-09-01 01:01:29 -0600

    Added: helmet-react-async

--- src/Pages/Xiao.jsx
+++ src/Pages/Xiao.jsx
@@ -1,12 +1,19 @@
+import { Helmet } from "react-helmet-async"
 import Sidebar from "../Components/Organisms/Sidebar"
 import SectionXiao from "../Components/Organisms/Xiao/SectionXiao"
 
 function Xiao() {
   return (
+    <>
+    <Helmet>
+      <title>Xiao</title>
+      <meta name="description" content="Xiao page description" />
+    </Helmet>
     <div className="w-screen">
         <Sidebar />
         <SectionXiao />
     </div>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <HelmetProvider>
     <App />
-  </StrictMode>,
-)
+  </HelmetProvider>
+);
